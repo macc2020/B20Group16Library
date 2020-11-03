ba95e8c3cee8bc9f3cd0a045a6bc8b4c22ffaafc
macc2020 <68972601+macc2020@users.noreply.github.com>
2020-11-03 17:50:14 -0600

    Macc2020 Test Added2

--- src/test/java/com/library/runners/CukesRunner.java
+++ src/test/java/com/library/runners/CukesRunner.java
@@ -8,8 +8,8 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "com/library/step_definitions",
-        dryRun = false,
-        tags = "@HeaderVerification"
+        dryRun = true,
+        tags = "@Macc2020Test"
 )
 
 public class CukesRunner {
